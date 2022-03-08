b29db71099b386f3d61698609b4bcba5368f96c5
Bernardo <bernardo.tg94@gmail.com>
2022-03-08 12:19:03 -0600

    agregadocumento

--- Microformas/gridJSON.cs
+++ Microformas/gridJSON.cs
@@ -27,30 +27,35 @@ namespace Microformas
 
                 string miJson = File.ReadAllText("ubicaciones.txt");//se carga archivo  con estructura json
 
-                var oUbicacion = JsonConvert.DeserializeObject<DTO_Ubicaciones>(miJson);//se deseraliza en clase modelo
-
-                //se prepara grid
-                grdJson.Columns.Clear();
-                grdJson.Columns.Add("city", "Ciudad");
-                grdJson.Columns.Add("description", "Descripcion");
-                grdJson.Columns.Add("latitude", "latitude");
-                grdJson.Columns.Add("longitude", "longitude");
-                grdJson.Columns["description"].SortMode = DataGridViewColumnSortMode.NotSortable;
-                grdJson.Columns["latitude"].SortMode = DataGridViewColumnSortMode.NotSortable;
-                grdJson.Columns["longitude"].SortMode = DataGridViewColumnSortMode.NotSortable;
-                grdJson.Columns["description"].ReadOnly = true;
-                grdJson.Columns["latitude"].ReadOnly = true;
-                grdJson.Columns["longitude"].ReadOnly = true;
-                grdJson.Columns["city"].ReadOnly = true;
-
-                foreach (var item in oUbicacion.ubicaciones)//se carga grid
+                if (miJson != null && miJson != "")
                 {
-                    grdJson.Rows.Add(item.city, item.description, item.latitude,item.longitude);
+                  
+                    var oUbicacion = JsonConvert.DeserializeObject<DTO_Ubicaciones>(miJson);//se deseraliza en clase modelo
+
+                    //se prepara grid
+                    grdJson.Columns.Clear();
+                    grdJson.Columns.Add("city", "Ciudad");
+                    grdJson.Columns.Add("description", "Descripcion");
+                    grdJson.Columns.Add("latitude", "latitude");
+                    grdJson.Columns.Add("longitude", "longitude");
+                    grdJson.Columns["description"].SortMode = DataGridViewColumnSortMode.NotSortable;
+                    grdJson.Columns["latitude"].SortMode = DataGridViewColumnSortMode.NotSortable;
+                    grdJson.Columns["longitude"].SortMode = DataGridViewColumnSortMode.NotSortable;
+                    grdJson.Columns["description"].ReadOnly = true;
+                    grdJson.Columns["latitude"].ReadOnly = true;
+                    grdJson.Columns["longitude"].ReadOnly = true;
+                    grdJson.Columns["city"].ReadOnly = true;
+
+
+                    foreach (var item in oUbicacion.ubicaciones)//se carga grid
+                    {
+                        grdJson.Rows.Add(item.city, item.description, item.latitude,item.longitude);
+                    }
+                    //se acomoda tamaño de columnas
+                    grdJson.AutoResizeColumns();
+                    grdJson.Columns["city"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                    grdJson.Columns["description"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
                 }
-                //se acomoda tamaño de columnas
-                grdJson.AutoResizeColumns();
-                grdJson.Columns["city"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-                grdJson.Columns["description"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
             }
             catch (Exception ex)
             {
